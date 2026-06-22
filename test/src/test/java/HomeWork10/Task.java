e5f63940591055879b7ef8a2fe2e1d63fb23b544
U_M2ACD <sergey456859@yandex.ru>
2026-06-22 14:22:04 +0300

    Создал логику работы с задачей. Класс Task. Создал конструктор, поля и методы работы с ними

--- test/src/test/java/HomeWork10/Task.java
+++ test/src/test/java/HomeWork10/Task.java
@@ -1,4 +1,39 @@
 package HomeWork10;
 
+import java.text.MessageFormat;
+
 public class Task {
+
+    private String nameTask;
+    private boolean completed;
+
+    public Task(String nameTask, boolean completed) {
+        this.nameTask = nameTask;
+        this.completed = completed;
+    }
+
+    public String getNameTask() {
+        return nameTask;
+    }
+
+    public void setNameTask(String nameTask) {
+        this.nameTask = nameTask;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    @Override
+    public String toString() {
+        if (completed) {
+            return MessageFormat.format("[x] {0}", nameTask);
+        } else {
+            return MessageFormat.format("[_] {0}", nameTask);
+        }
+    }
 }
