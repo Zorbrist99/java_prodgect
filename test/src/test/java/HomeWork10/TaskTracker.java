65b1d33b0d4a61bef12b01bfef63f2cc06cbfe3f
U_M2ACD <sergey456859@yandex.ru>
2026-06-22 14:25:39 +0300

    Написал основную логику для работы с листом задач (TaskTracker). Создал конструктор, поля и методы для работы с ними

--- test/src/test/java/HomeWork10/TaskTracker.java
+++ test/src/test/java/HomeWork10/TaskTracker.java
@@ -1,4 +1,23 @@
 package HomeWork10;
 
+import java.util.ArrayList;
+
 public class TaskTracker {
+    private ArrayList<Task> listTasks;
+
+    public TaskTracker() {
+        this.listTasks = new ArrayList<>();
+    }
+
+    public ArrayList<Task> getListTasks() {
+        return listTasks;
+    }
+
+    public void setListTasks(Task listTasks) {
+        this.listTasks.add(listTasks);
+    }
+
+    public void getTaskByName(String nameTask) {
+
+    }
 }
